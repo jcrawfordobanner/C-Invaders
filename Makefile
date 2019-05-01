4444056fea653c8dcddb6d1caaac8a41d5900fb2
jcrawfordobanner <jordan.crawford-obanner@students.olin.edu>
2019-05-01 13:24:18 -0400

    sad

--- Makefile
+++ Makefile
@@ -1,2 +1,5 @@
 tutorial2: tut2.c
 	gcc glad.o glad/glad.h tut2.c -o tutorial2 -lGL -lglfw -ldl
+
+++: plustest.cpp
+	g++ -o ++ glad.o glad/glad.h plustest.cpp -std=c++0x  -lGL -lglfw -ldl
--- tut2.c
+++ tut2.c
@@ -68,14 +68,14 @@ int main(int argc, char** argv){
 
 
   float vertices[] = {
-       0.5f,  0.5f, 0.0f,  // top right
+       0.0f,  0.5f, 0.0f,  // top right
        0.5f, -0.5f, 0.0f,  // bottom right
       -0.5f, -0.5f, 0.0f,  // bottom left
-      -0.5f,  0.5f, 0.0f   // top left
+      -0.5f, -0.5f, 0.0f   // top left
   };
   unsigned int indices[] = {  // note that we start from 0!
       0, 1, 3,   // first triangle
-      1, 2, 3    // second triangle
+      //1, 2, 3    // second triangle
   };
 
   unsigned int VBO, VAO, EBO;
