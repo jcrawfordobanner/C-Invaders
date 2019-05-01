2a03cd171c177785ae4c1824995bce3b8f58d719
dberny <diego.berny@students.olin.edu>
2019-05-01 13:12:23 -0400

    modified input

--- Makefile
+++ Makefile
@@ -2,7 +2,7 @@ tutorial2: tut2.c
 	gcc glad.o glad/glad.h tut2.c -o tutorial2 -lGL -lglfw -ldl
 
 input: input.c
-	gcc -o input input.c -lncurses
+	gcc -o input input.c
 
 changeColor: changeColor.c
 	gcc glad.o glad/glad.h changeColor.c -o changeColor -lGL -lglfw -ldl -lncurses
--- inputs/cppInput.cpp
+++ inputs/cppInput.cpp
@@ -24,23 +24,5 @@ int main() {
 		if (ch == 97) {
 			printf("you hit a\n");
 	}
-
-	  // int i,row,column;
-	  // //getting position of cursor by getyx function
-	  // for (i=column; i<=last_column; i+=2)
-	  // {
-	  //   //time interval of 1 sec
-	  //
-	  //   mvprintw(row,b,"<");   //print < in given (b,row) coordinates
-	  //
-	  //   //time interval of 1 sec
-	  //
-	  //   mvprintw(row,(b+1),"O");  //print "O" next to "<"
-	  //   int h = getch();   //to give the option for pressing another key
-	  //   if (h != KEY_RIGHT)  //break current loop if another key is pressed
-	  //   {
-	  //     break;
-	  //   }
-	  // }
 	}
 }
